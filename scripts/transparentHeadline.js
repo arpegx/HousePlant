a1b59f24b7cdaa7e883f72222ec8c504b1e570e4
larbaer <lorbaer@posteo.de>
2023-01-20 09:40:28 +0100

    On branch head-trans  Changes to be committed: 	modified:   html/index.html 	new file:   scripts/transparentHeadline.js 	modified:   styles/index.css  Description:     - Adding a transparent Headline.
    Issue:
        - While Resizing the window on left side there is some gap
        in cause of rendering issues ?

---
+++ scripts/transparentHeadline.js
@@ -0,0 +1,10 @@
+// Get Informations from DOM
+const canv = document.getElementById('main-headline-text');
+var ctx = document.getElementById('main-headline-text').getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0,0, canv.width + 10, canv.height +10);
+
+// Transparent Headline
+ctx.globalCompositeOperation = 'destination-out';
+ctx.font = "Bold 50px 'Helvetica'";
+ctx.fillText("House Plant",5 ,canv.height / 1.6);
