c4a3faee0dbc3a85647b8289c6b7bf9ddb50a503
larbaer <lorbaer@posteo.de>
2023-01-20 10:33:43 +0100

    On branch dev  Changes to be committed: 	modified:   scripts/transparentHeadline.js  Description     - Changing Font of transparent headline

--- scripts/transparentHeadline.js
+++ scripts/transparentHeadline.js
@@ -6,5 +6,5 @@ ctx.fillRect(0,0, canv.width + 10, canv.height +10);
 
 // Transparent Headline
 ctx.globalCompositeOperation = 'destination-out';
-ctx.font = "Bold 50px 'Helvetica'";
+ctx.font = "Bold 50px 'Copperplate'";
 ctx.fillText("House Plant",5 ,canv.height / 1.6);
